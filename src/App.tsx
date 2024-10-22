29489f57bb89a2b32d566936aff224cd2a79d4a2
Matheus7p <matheus.costa.7p@gmail.com>
2024-10-21 21:16:35 -0300

    feat: product and bid

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,64 @@
-import './App.css'
-import { Navbar } from './components/navbar.component'
+import { useState } from "react";
+import styles from "./App.module.css";
+import { Navbar } from './components/navbar.component';
 
 function App() {
+  const [bid, setBid] = useState(''); 
+  const [errorMessage, setErrorMessage] = useState(''); 
+  const [successMessage, setSuccessMessage] = useState(''); 
+  const minBid = 5000; 
+
+  const handleBidChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBid(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    const bidValue = parseFloat(bid);
+    if (isNaN(bidValue) || bidValue < minBid) {
+      setErrorMessage(`O lance deve ser no mínimo R$${minBid}.`);
+      setSuccessMessage('');
+    } else {
+      setSuccessMessage(`Lance de R$${bidValue} recebido com sucesso!`);
+      setErrorMessage('');
+    }
+  };
 
   return (
     <>
       <Navbar />
+      <section className={styles.section}>
+        <div className={styles.form}>
+          <h2><span>Rolex</span></h2>
+          <p className={styles.description}>
+            Um ícone de luxo e precisão, o Rolex é conhecido mundialmente por sua excelência e elegância atemporais.
+          </p>
+          <div className={styles.bar}></div>
+
+          
+          <label htmlFor="bid" className={styles.bidLabel}>Insira o valor do lance:</label>
+          <input
+            type="number"
+            id="bid"
+            value={bid}
+            onChange={handleBidChange}
+            className={styles.bidInput}
+            placeholder={`Lance mínimo: R$${minBid}`}
+          />
+          
+         
+          <button onClick={handleSubmit} className={styles.bidButton}>Enviar Lance</button>
+
+          
+          {errorMessage && <p className={styles.errorMessage}>{errorMessage}</p>}
+          {successMessage && <p className={styles.successMessage}>{successMessage}</p>}
+        </div>
+
+        <div className={styles.img}>
+          <img src="rolex.png" alt="rolex" />
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.component.tsx
+++ src/components/Navbar.component.tsx
@@ -6,10 +6,10 @@ export const Navbar = () => {
       <h1><span>Gentleman's Gear</span></h1>
       
         <div className={styles.links}>
-          <h3>Home</h3>
-          <h3><span>Products</span></h3>
-          <h3>Gallery</h3>
-          <h3>About</h3>    
+          <h3>Início</h3>
+          <h3><span>Produto</span></h3>
+          <h3>Galeria</h3>
+          <h3>Sobre</h3>    
         </div>
     </nav>
   );
